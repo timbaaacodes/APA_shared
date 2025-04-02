262e37e3ae1f25eeee3ed77e894926c01bf26bd9
Timba <timb14a@gmail.com>
2025-04-02 09:37:43 +0100

    New function of print added

---
+++ ind_modules.py
@@ -0,0 +1,7 @@
+# Individual module
+# this module contains self-created functions
+
+
+# prints package version - TEST
+def print_version(package):
+    print(f'The current version of {package.name} is {package.__version__}')
