0e34042f751cdb9be1befa0f401e969a5e4dd3d4
Tim Bajramaj <s-tbajramaj@ucp.pt>
2025-05-22 09:54:28 +0100

    Delete ind_modules.py

--- ind_modules.py
+++
@@ -1,9 +0,0 @@
-# Individual module
-# this module contains self-created functions
-
-
-# prints package version - TEST
-def print_version(package):
-    print(f'The current version of {package.name} is {package.__version__}')
-
-#test 2.0
